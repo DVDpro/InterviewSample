85e7b805f881107c5bbb374e4ca59db4f3164f7a
DVDpro <dvdpro@seznam.cz>
2019-05-12 21:05:32 +0200

    add identity service and tests

---
+++ Services/IdentityService/Identity.Test/DiscoveryEndpointTest.cs
@@ -0,0 +1,34 @@
+﻿using Identity;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+using System.Net.Http;
+using IdentityModel.Client;
+
+namespace Identity.Test
+{
+    public class DiscoveryEndpointTest : IClassFixture<WebApplicationFactory<Startup>>
+    {
+        private readonly WebApplicationFactory<Startup> _factory;
+
+        public DiscoveryEndpointTest(WebApplicationFactory<Startup> factory)
+        {
+            _factory = factory;
+        }
+
+        [Fact]
+        public async Task GetDiscoverySuccess()
+        {
+            var client = _factory.CreateClient();
+            var response = await client.GetDiscoveryDocumentAsync();
+            Assert.Equal(System.Net.HttpStatusCode.OK, response.StatusCode);
+            Assert.Equal("http://localhost/connect/authorize", response.AuthorizeEndpoint);
+            Assert.Equal("http://localhost/connect/token", response.TokenEndpoint);
+        }
+
+    }
+}
---
+++ Services/IdentityService/Identity/IdentityServerStaticConfiguration.cs
@@ -0,0 +1,59 @@
+﻿using IdentityServer4;
+using IdentityServer4.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Identity
+{
+    public class IdentityServerStaticConfiguration
+    {
+        public static IEnumerable<IdentityResource> GetIdentityResources()
+        {
+            return new List<IdentityResource>
+            {
+                new IdentityResources.OpenId(),
+                new IdentityResources.Profile(),
+                new IdentityResources.Email(),
+            };
+        }
+
+        private const string DataApiResourceKey = "data";
+
+        public static IEnumerable<ApiResource> GetApiResources()
+        {
+            return new List<ApiResource>
+            {
+                new ApiResource(DataApiResourceKey, "Data.Api")
+            };
+        }
+
+        public static IEnumerable<Client> GetClients()
+        {
+            return new List<Client>
+            {
+                new Client
+                {
+                    ClientId = "api.client.spa",
+                    AllowedGrantTypes = GrantTypes.Code,
+                    ClientSecrets =
+                    {
+                        new Secret("p@ssw0rd".Sha256()) //TODO: load client secrets from configuration (in dev: secret.json)
+                    },
+                    RequirePkce = true,
+                    RedirectUris =           { "http://localhost", "http://localhost:4200", "https://localhost:44306" },
+                    PostLogoutRedirectUris = { "http://localhost", "http://localhost:4200", "https://localhost:44306" },
+                    AllowedCorsOrigins =     { "http://localhost", "http://localhost:4200", "https://localhost:44306" },
+                    AllowedScopes = {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Email,
+                        IdentityServerConstants.StandardScopes.Profile,
+                        DataApiResourceKey
+                    }
+                }
+            };
+        }
+    }
+
+}
